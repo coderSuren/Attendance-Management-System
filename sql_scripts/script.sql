76babbba3533881345ab672951f1fc3e6628db3f
rtarun9 <rtarun0913@gmail.com>
2023-05-02 12:02:01 +0530

    Updated SQL script

--- sql_scripts/script.sql
+++ sql_scripts/script.sql
@@ -1,2 +1,9 @@
+CREATE TABLE system_database (
+    student_id VARCHAR(255),
+    password VARCHAR(255)
+);
 
-SELECT * from system_database where student_id='S001';
+INSERT INTO system_database (student_id, password) VALUES ('S001', 'test');
+INSERT INTO system_database (student_id, password) VALUES ('S002', 'test2');
+INSERT INTO system_database (student_id, password) VALUES ('S003', 'test3');
+INSERT INTO system_database (student_id, password) VALUES ('S004', 'test4'); 	
